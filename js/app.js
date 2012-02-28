530d1f4cbd975bb0ab88ee9dc8e9bc1b775a5ecd
DJ <johnson@developmentseed.org>
2012-02-28 16:18:53 -0500

    trashed jekyll, site redesign

--- js/app.js
+++ js/app.js
@@ -1,38 +1,51 @@
-var m, interaction;
+var m;
 var mm = com.modestmaps;
-var baselayer = 'djohnson.world-dark';
-var borders = 'mapbox.world-borders-light';
+var baselayer = 'mapbox.world-bright';
+var borders = '';
 var subNationalPointData = '';
 var activelayer = 'djohnson.final-superpac';
+var activedescrip = $('.description.active');
 var layers = [
         baselayer,
         borders,
         activelayer
     ];
 
-_.each(['totals','obama','romney', 'gingrich', 'paul', 'santorum'], function(candidate) {
 
-    $('#' + candidate).click(function(e) {
-       
-        $('.description').hide();
-        $('#' + candidate + '-description').show();
+    _.each(['totals','obama','romney', 'gingrich', 'paul', 'santorum'], function(candidate) {
+
+        $('#' + candidate).click(function(e) {
+
+            $('.description').hide();
+            $('#' + candidate + '-description').show();
+        });
     });
-});
+
 
 
 wax.tilejson('http://api.tiles.mapbox.com/v2/' + layers + '.jsonp', function(tilejson) {
-    tilejson.minzoom = 3;
-      tilejson.maxzoom = 5;
+        tilejson.minzoom = 3;
+        tilejson.maxzoom = 5;
     m = new mm.Map('map', new wax.mm.connector(tilejson), null, [
         new mm.MouseHandler(),
         new mm.TouchHandler()
         ]
        
     );
-    m.setCenterZoom(new mm.Location(32,-95), 4);
-     tilejson.attribution = 'Powered by open source <a href="http://tilemill.com" target="_blank"> TileMill</a> ';
-        wax.mm.legend(m, tilejson).appendTo(m.parent);
-        interaction = wax.mm.interaction(m, tilejson);
+        m.setCenterZoom(new mm.Location(38,-76), 4);
+        tilejson.attribution = 'Maps made with open source <a href="http://tilemill.com" target="_blank"> TileMill</a>.  <a href="http://reporting.sunlightfoundation.com/super-pacs/file-downloads/">Get the Data</a> from the Sunlight Foundation.'
+        ;
+        
+        myTooltip = new wax.tooltip;
+        myTooltip.getTooltip = function(feature, context) {
+            return $('#tooltips').html(feature).get(2);
+        }
+        myTooltip.hideTooltip = function(feature, context) {
+            //$('#tooltips').html('<p class="message">' + emptyTooltip + '</p>');
+            $('#tooltips').html('');
+        }
+
+        wax.mm.interaction(m, tilejson, {callbacks: myTooltip,});
         wax.mm.attribution(m, tilejson).appendTo(m.parent);
         wax.mm.zoomer(m, tilejson).appendTo($('#controls')[0]);
         wax.mm.bwdetect(m, {
@@ -43,19 +56,26 @@ wax.tilejson('http://api.tiles.mapbox.com/v2/' + layers + '.jsonp', function(til
    
 
 
+
 function refreshMap(layers) {
        wax.tilejson('http://api.tiles.mapbox.com/v2/' + layers + '.jsonp', function(tilejson) {
+              interaction = wax.mm.interaction(m, tilejson, {callbacks: myTooltip,})
+               interaction.remove();
+                interaction = wax.mm.interaction(m, tilejson, {callbacks: myTooltip,});;
               tilejson.minzoom = 3;
               tilejson.maxzoom = 5;
               m.setProvider(new wax.mm.connector(tilejson));
-              $('.wax-legends').remove();
-              wax.mm.legend(m, tilejson).appendTo(m.parent);
-              interaction.remove();
-              interaction = wax.mm.interaction(m, tilejson);
+            
+             
           });
     }
 
 // TODO: Change this
+
+
+
+
+
 $(document).ready(function () {
 
     // Layer Selection
@@ -64,8 +84,11 @@ $(document).ready(function () {
         if (!$(this).hasClass('active')) {
             $('ul li a').removeClass('active');
             
+        
+            
             $(this).addClass('active');
-     
+            
+            
             var activelayer = $(this).attr('data-layer');
             layers = [
                 baselayer,
@@ -91,7 +114,7 @@ $(document).ready(function () {
 
         var center = m.pointLocation(new mm.Point(m.dimensions.x/2,m.dimensions.y/2));
         var embedUrl = 'http://api.tiles.mapbox.com/v2/' + layers + '/mm/zoompan,tooltips,legend,bwdetect.html#' + m.coordinate.zoom +
-                        '/' + center.lat + '/' + center.lon;
+                         '/' + center.lat + '/' + center.lon;
         $('#embed-code-field textarea').attr('value', '<iframe src="' + embedUrl +
             '" frameborder="0" width="650" height="500"></iframe>');
 
