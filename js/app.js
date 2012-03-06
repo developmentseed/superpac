fc71a3ad24afa8ae20aad59f458ac4ecece86ce0
DJ <johnson@developmentseed.org>
2012-03-06 15:09:57 -0500

    final updates

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var m, interaction, mm = com.modestmaps;
 var baselayer = 'mapbox.world-blank-bright';
 var borders = 'djohnson.usa_borders';
-var activelayer = 'djohnson.all_superpacs';
+var activelayer = 'djohnson.superpacs-march5-all';
 var layers = [
         baselayer,
         borders,
@@ -19,7 +19,7 @@ wax.tilejson('http://api.tiles.mapbox.com/v2/' + layers + '.jsonp', function(til
             ]
     );
         m.setCenterZoom(new mm.Location(38,-76), 4);
-        tilejson.attribution = 'Maps made with open source <a href="http://tilemill.com" target="_blank"> TileMill</a>.  <a href="http://reporting.sunlightfoundation.com/super-pacs/file-downloads/"> Data</a> from the Sunlight Foundation from <a href="http://www.fec.gov/data/IndependentExpenditure.do?format=html&election_yr=2012"/>FEC</a>, Februrary 22, 2012.<br /> Data covers November 22, 2011 to February 22, 2012.';
+        tilejson.attribution = 'Maps made with open source <a href="http://tilemill.com" target="_blank"> TileMill</a>.  <a href="http://reporting.sunlightfoundation.com/super-pacs/file-downloads/"> Data</a> from the Sunlight Foundation from <a href="http://www.fec.gov/data/IndependentExpenditure.do?format=html&election_yr=2012"/>FEC</a>, collected March 5, 2012.<br /> Data covers November 22, 2011 to March 5, 2012.';
 
         myTooltip = new wax.tooltip;
         myTooltip.getTooltip = function(feature, context) {
@@ -53,6 +53,9 @@ function refreshMap(layers) {
                tilejson.minzoom = 3;
                tilejson.maxzoom = 5;
                m.setProvider(new wax.mm.connector(tilejson));
+               window.setTimeout(function() {
+                 b.setProvider(new wax.mm.connector(tilejson));
+               }, 500);
                $('#tooltips').empty();
                interaction.remove();
                legend = wax.mm.legend(m, tilejson).appendTo(document.getElementById('tooltips'));
@@ -92,28 +95,28 @@ $('ul li a').click(function (e) {
         }
     });
 
-    // Embed Code
-    $('a.share').click(function(e){
-        e.preventDefault();
-        $('#share, #overlay').addClass('active');
-
-        var twitter = 'http://twitter.com/intent/tweet?status=' +
-        '1,000 Days Interactive Map ' + encodeURIComponent(window.location);
-        var facebook = 'https://www.facebook.com/sharer.php?t=1000%20Days%20Interactive%20Map&u=' +
-        encodeURIComponent(window.location);
-
-        document.getElementById('twitter').href = twitter;
-        document.getElementById('facebook').href = facebook;
-
-        var center = m.pointLocation(new mm.Point(m.dimensions.x/2,m.dimensions.y/2));
-        var embedUrl = 'http://api.tiles.mapbox.com/v2/' + layers + '/mm/zoompan,tooltips,legend,bwdetect.html#' + m.coordinate.zoom +
-                         '/' + center.lat + '/' + center.lon;
-        $('#embed-code-field textarea').attr('value', '<iframe src="' + embedUrl +
-            '" frameborder="0" width="650" height="500"></iframe>');
-
-        $('#embed-code')[0].tabindex = 0;
-        $('#embed-code')[0].select();
-    });
+// Embed Code
+$('a.share').click(function(e){
+     e.preventDefault();
+     $('#share, #overlay').addClass('active');
+     
+     var twitter = 'http://twitter.com/intent/tweet?status=' +
+     'Funding the Hate Campaigns with SuperPAC Spending ' + encodeURIComponent(window.location);
+     var facebook = 'https://www.facebook.com/sharer.php?t=1000%20Days%20Interactive%20Map&u=' +
+     encodeURIComponent(window.location);
+     
+     document.getElementById('twitter').href = twitter;
+     document.getElementById('facebook').href = facebook;
+     
+     var center = m.pointLocation(new mm.Point(m.dimensions.x/2,m.dimensions.y/2));
+     var embedUrl = 'http://api.tiles.mapbox.com/v2/' + layers + '/mm/zoompan,tooltips,legend,bwdetect.html#' + m.coordinate.zoom +
+                      '/' + center.lat + '/' + center.lon;
+     $('#embed-code-field textarea').attr('value', '<iframe src="' + embedUrl +
+         '" frameborder="0" width="650" height="500"></iframe>');
+     
+     $('#embed-code')[0].tabindex = 0;
+     $('#embed-code')[0].select();
+  });
 
     // Trigger close buttons with the escape key
     $(document.documentElement).keydown(function (e) {
